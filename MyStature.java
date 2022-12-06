44cf5548c9033c98eb69b855b14c329815d75128
Latifsai <117080810+Latifsai@users.noreply.github.com>
2022-12-06 22:08:53 +0100

    Add files via upload

---
+++ MyStature.java
@@ -0,0 +1,27 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class MyStature {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите желаемою высоты вашей ели:");
+        int treeHeight = in.nextInt();
+
+        for (int i = 0; i <= treeHeight; i++) {
+            for (int j = treeHeight; j > i ; j--) {
+                System.out.print(" ");
+            }
+            for (int k = i; k >=1; k--) {
+                System.out.print(" ^");
+            }
+            System.out.println();
+        }
+
+
+
+    }
+}
+
+
+
